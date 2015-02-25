4fcc6a3be3232428ae0b32222412b6dbf20743e8
Richard Bronosky <richard@bronosky.com>
2015-02-25 03:08:32 -0500

    Allow test to accept arguments.

--- celery_test/test.py
+++ celery_test/test.py
@@ -1,6 +1,13 @@
+from sys import argv
 from celery_test.tasks import add
 
 def main():
-    res = add.delay(4, 4)
+    val = [4, 4]
+    for i in range(2):
+        try:
+            val[i] = int(argv[i+1])
+        except (ValueError,IndexError):
+            pass
+    res = add.delay(*val)
     res.get(timeout=1)
-    print "result: {out}".format(out=res.result)
+    print "{val0} + {val1} = {out}".format(val0=val[0], val1=val[1], out=res.result)
