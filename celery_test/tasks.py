5350f1d09bda6432321a72632c25daf1b1695f2d
Richard Bronosky <richard@bronosky.com>
2015-03-03 11:02:50 -0500

    moar verbosity

--- celery_test/tasks.py
+++ celery_test/tasks.py
@@ -1,7 +1,8 @@
 import os
 from celery import Celery
 
-app = Celery('tasks', backend=os.environ['CELERY_RESULT_BACKEND'], broker=os.environ['BROKER_URL'])
+app = Celery('tasks')
+app.config_from_object("celeryconfig")
 
 @app.task
 def add(x, y):
